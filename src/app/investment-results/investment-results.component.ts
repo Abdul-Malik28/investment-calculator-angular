04cb2c3f0c4dc583f1ce67ff51b0cc32fac7f775
Abdul_Malik <malik.info28@gmail.com>
2025-01-08 18:50:48 +0530

    accesing the userinput values and rendering it to the DOM

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,29 @@
+import { Component, /*input,*/ Input } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+  
+  /* results = input<{
+    year: number,
+    interest: number,
+    valueEndOfYear: number,
+    annualInvestment: number,
+    totalInterest: number,
+    totalAmountInvested: number
+  }[]>(); */
+  
+  @Input({ required: true }) results?: {
+    year: number,
+    interest: number,
+    valueEndOfYear: number,
+    annualInvestment: number,
+    totalInterest: number,
+    totalAmountInvested: number
+  }[];
+}
